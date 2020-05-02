476e8a80866cc0e679241e661dc9f54761906a17
Nguyen Thanh <dat.nguyen99@hcmut.edu.vn>
2020-05-02 22:22:11 +0700

    3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('', include('homepage.urls')),
+    path('des/', include('des.urls')),
 ]
