e1a0641201541f31e7d311a59ef2813cb548d7ad
Nguyen Thanh <dat.nguyen99@hcmut.edu.vn>
2020-05-03 15:22:52 +0700

    4

--- mysite/des/views.py
+++ mysite/des/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+
+
+def des(request):
     return render(request,'des/des.html')
